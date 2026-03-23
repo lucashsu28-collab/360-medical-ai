72829d2a644a6c9ecbe837f516e7049a5e5878ce
lucashsu28-collab <lucashsu28@gmail.com>
2026-03-23 10:27:46 +0800

    fix: 清除clinic_reviews重複資料

--- check_db.py
+++ check_db.py
@@ -1,8 +1,6 @@
 import psycopg2
 conn=psycopg2.connect(host='34.81.74.228',dbname='medical_ai',user='postgres',password='Medical360@2026')
 cur=conn.cursor()
-cur.execute("SELECT COUNT(*) FROM clinics WHERE google_place_id IS NOT NULL AND google_place_id != ''"
-)
-print('with_place_id:', cur.fetchone()[0])
-cur.execute("SELECT COUNT(*) FROM clinics")
-print('total:', cur.fetchone()[0])
+cur.execute('DELETE FROM clinic_reviews WHERE id NOT IN (SELECT MIN(id) FROM clinic_reviews GROUP BY clinic_id, author_name, text)')
+print('deleted:', cur.rowcount)
+conn.commit()
